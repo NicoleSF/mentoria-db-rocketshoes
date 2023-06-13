0c6eac455d65cbf9d9e93f3649683d609324da17
Nicole Siqueira Fagundes <nicole_fagundes@sicredi.com.br>
2023-06-13 20:04:07 -0300

    feat: aprendendo autorização/autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { OidcProvider } from "@axa-fr/react-oidc";
 import { CartContextProvider } from "./context/CartContex";
 import { Routers } from "./routes";
 
+const configuration = {
+  client_id: "interactive.public.short",
+  redirect_uri: window.location.origin + "/authentication/callback",
+  silent_redirect_uri:
+    window.location.origin + "/authentication/silent-callback",
+  scope: "openid profile email api offline_access", // offline_access scope allow your client to retrieve the refresh_token
+  authority: "https://demo.duendesoftware.com",
+  service_worker_relative_url: "/OidcServiceWorker.js",
+  service_worker_only: false,
+};
+
 function App() {
   return (
-    <CartContextProvider>
-      <div>
-        <Routers />
-      </div>
-    </CartContextProvider>
+    <OidcProvider configuration={configuration}>
+      <CartContextProvider>
+        <div>
+          <Routers />
+        </div>
+      </CartContextProvider>
+    </OidcProvider>
   );
 }
 
